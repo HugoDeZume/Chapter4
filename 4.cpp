2b1b745dfe847d34e1ca96278e47776ce63c357e
HugoDeZume <62634400+HugoDeZume@users.noreply.github.com>
2021-02-14 15:46:21 -0600

    Delete 4.cpp

--- 4.cpp
+++
@@ -1,7 +0,0 @@
-#include<conio.h>
-#include<stdio.h>
-int main(void){
-	printf("H   H  EEEEE  L      L      OOOOO\nH   H  E      L      L      O   O\nH   H  E      L      L      O   O\nHHHHH  EEEEE  L      L      O   O\nH   H  E      L      L      O   O\nH   H  E      L      L      O   O\nH   H  EEEEE  LLLLL  LLLLL  OOOOO ");
-	
-	return 0;
-}
